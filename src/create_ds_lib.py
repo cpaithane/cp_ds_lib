f669555c7b90423e3e0ddd17043d8ed3f0471248
CP <cpaithane@gmail.com>
2016-03-16 10:13:05 +0530

    Fixing bug in create_ds_lib.py

--- src/create_ds_lib.py
+++ src/create_ds_lib.py
@@ -5,10 +5,13 @@ import shutil
 
 SUBDIRS = ["bst", "dll", "queue", "scll", "sll", "stack"]
 
-shutil.rmtree("bin")
+if os.path.exists("bin"):
+	shutil.rmtree("bin")
+
 os.mkdir("bin")
 
 for subdir in SUBDIRS:
 	os.system("cp -f %s/*.o bin/." % (subdir))
 
 os.system("cp -f main.o bin/.")
+
